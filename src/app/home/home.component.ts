c630f612b6159f1498790f39585adddbf9469bd9
Anders Leung <anders.yb.leung@gmail.com>
2021-02-09 11:51:59 -0800

    Cleanup.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,12 +11,7 @@ import * as moment from 'moment';
 export class HomeComponent implements OnInit {
   date: string = moment('2020-01-20').format('MMM D, YYYY');
   messages: string[] = [
-    `Invoice PDFs that generate with a symbol similar to "Đ" have extra spaces at the end of their lines,
-    remove the extra spaces for each line in an invoice's service description to remove the symbol.`,
-    `When switching PR T4 clients to One Time T4 clients, or vice versa, please simply toggle the "One Time
-    T4" checkbox and press save, this will create a new job for the newly converted One Time T4 or PR T4 client
-     - if there are old jobs still existing for the client's previous type, please contact Anders to remove it,
-     or simply add a remark that the job is void, and fill in the required fields for the job to be considered done.`
+    `Nothing here...`,
   ];
 
   constructor(
