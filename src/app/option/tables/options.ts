3f259599e0c58567a92fc94db508ec1430ee136e
Anders Leung <anders.yb.leung@gmail.com>
2021-02-09 13:07:58 -0800

    Trim down the table.

--- src/app/option/tables/options.ts
+++ src/app/option/tables/options.ts
@@ -1,84 +1,46 @@
-import { deleteRow } from 'src/app/globals';
-
 const table = (options) => {
   const { types } = options;
   const columns = [
     { label: 'Position', field: 'position', edit: { type: 'input' } },
     { label: 'Name', field: 'name', edit: { type: 'input' } },
-    { ...deleteRow },
-    { label: 'Hidden Details', field: 'details', visible: false },
   ];
-
-  const tables = {
-    tabs: [],
-    columns,
-    expandForm: [
+  const addForm: any = {
+    form: [
       [
-        { label: 'Details', field: 'details', textarea: true, rows: 5 },
+        { label: 'Type', field: 'type', type: 'autocomplete', options: types }
+      ],
+      [
+        { label: 'Position', field: 'position' },
+        { label: 'Name', field: 'name' },
       ],
     ],
   };
+  const tabs = [];
+  if (types.length === 0) {
+    tabs.push({
+      name: 'Default',
+      query: {},
+      addForm: {},
+    });
+  }
 
   types.forEach((type) => {
     let specificColumns = undefined;
-    let addForm: any = {
-      form: [
-        [
-          { label: 'Type', field: 'type', type: 'autocomplete', options: types }
-        ],
-        [
-          { label: 'Position', field: 'position' },
-          { label: 'Name', field: 'name' },
-        ],
-      ],
-      default: { type },
-    };
+    addForm.default = { type };
 
-    if (type === 'Industry') {
-      specificColumns = [...columns];
-      specificColumns.shift();
-
-      addForm.form = [
-        [
-          { label: 'Type', field: 'type', type: 'autocomplete', options: types }
-        ],
-        [
-          { label: 'Name', field: 'name' },
-        ],
-      ];
-    }
-
-    if (type === 'Description') {
-      specificColumns = [...columns];
-      specificColumns.splice(2, 0, {
-        label: 'Details',
-        field: 'expandShouldNotExist',
-        type: 'expandButton',
-        expand: () => true,
-        color: '#1976D2',
-      });
-
-      addForm.form = [
-        [
-          { label: 'Type', field: 'type', type: 'autocomplete', options: types }
-        ],
-        [
-          { label: 'Position', field: 'position' },
-          { label: 'Name', field: 'name' },
-        ],
-        [
-          { label: 'Data', field: 'data', textarea: true, rows: 15 },
-        ],
-      ];
-    }
-
-    tables.tabs.push({
+    tabs.push({
       name: type,
       query: { type },
       columns: specificColumns,
       addForm,
     });
   });
+  
+  const tables = {
+    tabs,
+    columns,
+    addForm,
+  };
 
   return tables;
 };
